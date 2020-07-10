337524d459d39005b4e60eebd2d849fa81682e16
2black0 <2black0@gmail.com>
2020-07-10 07:53:47 +0700

    add comment

--- software/arduino/main.ino
+++ software/arduino/main.ino
@@ -1,3 +1,4 @@
+// library yang dipakai
 #include <Arduino.h>
 #include <EMailSender.h>
 #include <ESP8266WiFi.h>
@@ -6,29 +7,30 @@
 #include <ESP8266HTTPClient.h>
 #include <SimpleTimer.h>
 
+// waktu pengiriman ke server 60000ms = 60s = 1m
 #define timersend 60000
 
+// ganti sesuai yang dipakai
+const char *ssid = "Wifi-Roboto";                                     // ganti wifi
+const char *password = "arDY1234";                                    // ganti password
+String url = "http://192.168.1.4/turbidity/save.php?x=";              // ganti ip komputer
+EMailSender emailSend("ianputrapratama809@gmail.com", "kenponx1976"); // ganti email sama password
+
+// variable yang dipakai
 bool debugSerial = 1;
 bool debugLCD = 1;
-
 const int analogInPin = A0;
 float sensorValue = 0;
-
 int i = 0;
 int clean_status = 0;
-
-const char *ssid = "Wifi-Roboto";
-const char *password = "arDY1234";
-String url = "http://192.168.1.4/turbidity/save.php?x=";
-
 uint8_t connection_state = 0;
 uint16_t reconnect_interval = 10000;
 
+// definisi instanse
 LiquidCrystal_I2C lcd(0x27, 16, 2);
-EMailSender emailSend("ianputrapratama809@gmail.com", "kenponx1976");
-
 SimpleTimer timer;
 
+// fungsi connect dgn wifi
 uint8_t WiFiConnect(const char *nSSID = nullptr, const char *nPassword = nullptr)
 {
   static uint16_t attempt = 0;
@@ -65,6 +67,7 @@ uint8_t WiFiConnect(const char *nSSID = nullptr, const char *nPassword = nullptr
   return true;
 }
 
+// fungsi menunggu koneksi wifi
 void Awaits()
 {
   uint32_t ts = millis();
@@ -79,6 +82,7 @@ void Awaits()
   }
 }
 
+// fungsi setup
 void setup()
 {
   Serial.begin(115200);
@@ -95,6 +99,7 @@ void setup()
   timer.setInterval(timersend, send_http);
 }
 
+// fungsi utama
 void loop()
 {
   timer.run();
@@ -102,32 +107,25 @@ void loop()
   serial_show("T:" + String(sensorValue) + "NTU", 1);
   lcd_show(1, "S:Running", 0, 0, "T:" + String(sensorValue) + "NTU", 0, 1, 1000);
 
-  if (sensorValue <= 20 && (clean_status == 1 || clean_status == 2)) // air bersih
+  if (sensorValue <= 20 && (clean_status == 1 || clean_status == 2)) // kondisi air bersih <=20
   {
     clean_status = 0;
   }
-  if (sensorValue > 20 && sensorValue <= 25 && clean_status == 0) // air cukup keruh
+  if (sensorValue > 20 && sensorValue <= 25 && clean_status == 0) // kondisi air cukup keruh >20 dan <=25
   {
     send_http();
     send_email(1);
     clean_status = 1;
   }
-  if (sensorValue > 25 && (clean_status == 0 || clean_status == 1)) // air sangat keruh
+  if (sensorValue > 25 && (clean_status == 0 || clean_status == 1)) // kondisi air sangat keruh >25
   {
     send_http();
     send_email(2);
     clean_status = 2;
   }
-  /*
-  read_analog();
-  //serial_show("T:" + String(sensorValue) + "NTU", 1);
-  //lcd_show(1, String(sensorValue), 0, 0, " ", 0, 1, 1);
-  //lcd_show(0, " ", 0, 0, String(sensorValue), 0, 1, 1);
-  lcd_show(1, "S:Running", 0, 0, "T:" + String(sensorValue) + "NTU", 0, 1, 1000);
-  //lcd_show(1, "A:" + String(sensorValue) + "V", 0, 0, "T:" + String(sensorValue) + "NTU", 0, 1, 1);
-  delay(5000);*/
 }
 
+// fungsi untuk menampilkan ke lcd
 void lcd_show(int clear, String text1, int x1, int y1, String text2, int x2, int y2, int waitms)
 {
   if (debugLCD == 1)
@@ -145,6 +143,7 @@ void lcd_show(int clear, String text1, int x1, int y1, String text2, int x2, int
   }
 }
 
+// fungsi untuk mengirim data serial
 void serial_show(String text, int newline)
 {
   if (debugSerial == 1)
@@ -160,6 +159,7 @@ void serial_show(String text, int newline)
   }
 }
 
+// fungsi untuk baca data analog
 void read_analog()
 {
   float totalValue = 0;
@@ -189,6 +189,7 @@ void read_analog()
   }
 }
 
+// fungsi untuk kirim ke database
 void send_http()
 {
   String x;
@@ -216,6 +217,7 @@ void send_http()
   }
 }
 
+// fungsi untuk kirim email
 void send_email(int status)
 {
   EMailSender::EMailMessage message;
@@ -245,12 +247,4 @@ void send_email(int status)
   serial_show("S:Send Email ", 0);
   serial_show("Status:" + status_email, 1);
   lcd_show(1, "S:Email", 0, 0, "Status:" + status_email, 0, 1, 1000);
-
-  //String response_status = String(resp.status);
-  //String response_code = String(resp.code);
-  //String response_desc = String(resp.desc);
-  //serial_show("Response Status:" + response_status, 1);
-  //serial_show("Response Code:" + response_code, 1);
-  //serial_show("Response Desc:" + response_desc, 1);
-  //lcd_show(1, response_status, 0, 0, response_code, 0, 1, 1000);
 }
